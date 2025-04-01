b35d7722e530c7468e8808ed28cbef6811a2e6ff
Parth <94096881+paarth0504@users.noreply.github.com>
2025-04-01 07:07:19 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,30 @@
+"use strict"; //Treats all Js code as Newer Version
+
+// alert(3+3) // we Are Using Js, Not Browser
+
+console.log(3
+    +
+    3)// Code Readablity Should Be High
+    
+    console.log("Parth")
+
+    //tc39 for Documentation
+    //Another One Is mdn
+
+    let name = "Parth"
+    let age = 23
+    let isLoggedIn = false
+    let state;
+
+    //number ==> 2 To Power 53
+    //bigint
+    //string ==>""
+    //boolean ==>true/false
+    //null ==> Its An Standalone Value 
+    //undefined
+    //symbol ==> unique
+
+    //object
+
+    console.log(typeof undefined);// Type Will Undefined
+    console.log(typeof null);// Type Will object
