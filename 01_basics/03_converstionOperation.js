609d73ac49ea4f6f3c1a6775ccf927971c298733
Parth <94096881+paarth0504@users.noreply.github.com>
2025-04-01 09:40:53 +0000

    Converstion of datatypes

---
+++ 01_basics/03_converstionOperation.js
@@ -0,0 +1,24 @@
+let score = "Parth"
+// console.log(typeof score);
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+//"33"==>>33
+//"33abc"==>>NAN - Not A Number But Its Type Is Number
+// true==>>1; false==>>0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1=> true; 0=> false
+//""=> false
+// "parth"=> true
+
+let someNumber=45
+let stringNumber = String(someNumber)
+console.log(someNumber);
+console.log(typeof stringNumber);
