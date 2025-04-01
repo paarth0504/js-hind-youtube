068901e84ab4e368bd22c74180f21794fe82b77a
Parth <94096881+paarth0504@users.noreply.github.com>
2025-04-01 11:38:35 +0000

    Convertion Operation

--- 01_basics/03_converstionOperation.js
+++ 01_basics/03_converstionOperation.js
@@ -20,5 +20,36 @@ let booleanIsLoggedIn= Boolean(isLoggedIn)
 
 let someNumber=45
 let stringNumber = String(someNumber)
-console.log(someNumber);
-console.log(typeof stringNumber);
+// console.log(someNumber);
+// console.log(typeof stringNumber);
+
+// *********operations***************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Parth"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+ 2); for now 1st is string so it get convt into string
+// console.log(1 + "2"); here 
+// console.log("1"+2+2);
+// console.log(1+2+"2"); First Numbers Will get Executed and then string will get printed
+
+// console.log(+ true);
+// console.log(+"");
+
+let gameCounter = 100
+gameCounter++;
+// console.log(gameCounter);
+
+
+
+
+
+
+
