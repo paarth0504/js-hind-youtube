fcf307a35a7c2dfef57dc82f5dfa469a5f285889
Parth <94096881+paarth0504@users.noreply.github.com>
2025-04-02 09:42:59 +0000

    Operations On primitive nd non p variable

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anoId= Symbol('123')
 
-console.log(id === anoId);
+// console.log(id === anoId);
 
 const bigNumber = 1641621646145n
 
@@ -28,12 +28,43 @@ let myObj ={
 }
 
 const myFunction= function(){
-    console.log("Hello World");  // A Function which is stored in Variable       
+    // console.log("Hello World");  // A Function which is stored in Variable       
     
 }      
 
-console.log(typeof anoId);
+// console.log(typeof anoId);
 
-console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
+
+// **********************************************************************************************************
+
+let myYoutubename = "Parth Deshmukh"
+
+let anotherName = "Only Cars"
+
+// console.log(myYoutubename);
+// console.log(anotherName);
+
+let userOne = {
+    email: "paarthdeshmukh71@gmail.com",
+    upi: "user@ybl"
+}
+// console.log(userOne);
+
+let userTwo = userOne
+
+userTwo.email = "onlycars24@gmail.com"
+// console.log(userTwo);
+// console.log(userOne);
+// console.log(userTwo);
+
+userTwo.email="helloworl@gmail.com"
+// userOne.upi = "customer@ybl"
+
+// console.log(userOne);
+
+
+
+
 
 
