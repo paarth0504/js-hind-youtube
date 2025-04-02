d0ae7e8080639fc6dcc6cbe731f55e2e3f483cf7
Parth <94096881+paarth0504@users.noreply.github.com>
2025-04-02 12:01:48 +0000

    Numbers and Maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,34 @@
+const score = 80
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.845
+// console.log(otherNumber.toPrecision(2));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ************+*******************+****MATHS**************+***************
+
+console.log(Math);
+console.log(Math.round(4.2));
+console.log(Math.abs(4));
+console.log(Math.ceil(4.23));
+console.log(Math.floor(4.32));
+
+// 15:18 done \\ video number -12
+
+
+
+
+
+
+
+
+
+
