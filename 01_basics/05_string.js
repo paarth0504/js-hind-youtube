0e6de6b6a69f1c00c57c6ee511a94955b5f1530e
Parth <94096881+paarth0504@users.noreply.github.com>
2025-04-02 10:59:30 +0000

    About Strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,35 @@
+const name = "Parth"
+const repoValue = 50
+
+
+
+// console.log(name + repoValue + "units");
+
+// console.log(`Hello My Name Is  ${name} And My Repo Value Is ${repoValue}`);
+
+ const gameName = new String('Parth Deshmukh')
+//  console.log(gameName.charAt(9));
+//  console.log(gameName.toUpperCase());
+//  console.log(gameName.length);
+//  console.log(gameName.indexOf('D'));
+
+ const newString = gameName.substring(0 , 5) // Substring Method
+//  console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+// console.log(anotherString);
+ 
+const newStringOne = "     Deshmukh"
+console.log(newStringOne);
+
+console.log(newStringOne.trim());
+
+ 
+ 
+ 
+ 
+ 
+
+
+
+
