6ff4df91b86db369088a0bf0774c77aa598362f5
Parth <94096881+paarth0504@users.noreply.github.com>
2025-04-03 12:14:01 +0000

    Array p-2

---
+++ 02_basic/02_arrays.js
@@ -0,0 +1,29 @@
+const marvel_heros= ["Thor", "Ironman", "Spiderman"]
+const dc_heros = ["Superman", "Flash", "Batman"]
+
+// const allHeroes = marvel_heros.concat(dc_heros)
+// marvel_heros.push(dc_heros)
+// console.log(allHeroes);
+const all_new_heros = [marvel_heros, dc_heros]
+// console.log(all_new_heros);
+
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+// console.log(Array.of(score1,score2,score3));
+
+// console.log(Array.isArray("PARTH"))
+// console.log(Array.from("Parth"));
+// console.log(Array.from({name: "Parth "}));
+
+//********Array Part 15 cmplted */
+
+
+
+
+
