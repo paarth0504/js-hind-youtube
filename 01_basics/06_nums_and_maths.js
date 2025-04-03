06195f90912a42f075418983d1fb7471394e2805
Parth <94096881+paarth0504@users.noreply.github.com>
2025-04-03 05:54:22 +0000

    Math and Nums

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -15,13 +15,26 @@ const hundreds = 1000000
 
 // ************+*******************+****MATHS**************+***************
 
-console.log(Math);
-console.log(Math.round(4.2));
-console.log(Math.abs(4));
-console.log(Math.ceil(4.23));
-console.log(Math.floor(4.32));
+// console.log(Math);
+// console.log(Math.round(4.2));
+// console.log(Math.abs(4));
+// console.log(Math.ceil(4.23));
+// console.log(Math.floor(4.32));
+// console.log(Math.min(4,5,98,1));
+// console.log(Math.max(45,65,84,23));
 
-// 15:18 done \\ video number -12
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max-min+1))+1);
+
+
+
+
 
 
 
