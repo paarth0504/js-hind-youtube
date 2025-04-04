a371fc2998c5f7fb35acf52c32e8a7119d88934b
Parth <94096881+paarth0504@users.noreply.github.com>
2025-04-04 09:52:05 +0000

    understanding Object

--- 02_basic/03_objects.js
+++ 02_basic/03_objects.js
@@ -1,13 +1,34 @@
+// Initiailzing An Object
 
+const newSymbo = Symbol("Key123332") //Creating A Symbol 
 const JsUser = {
     name : "Parth",
     age : 23,
     location : "Maharashtra",
     cont : 9049886898,
     email : "paarthdeshmukh71@gmail.com",
-    isLoggedIn: false
+    isLoggedIn: false,
+    [newSymbo] : ("Key12345")
 }
 
-console.log(JsUser.age);
+// console.log(JsUser.age);
+// console.log(JsUser.cont);
+console.log(JsUser[newSymbo]);
+
+JsUser.greeting = function (){
+    console.log(`Hello JS User,${this.name}`);
+    
+}
+console.log(JsUser.greeting());
+
+
+
+
+
+
+
+
+
+
 
 
