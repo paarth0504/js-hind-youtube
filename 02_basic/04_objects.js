0bb6726c4168251b9a7e7847d9684bb23a97a956
Parth <94096881+paarth0504@users.noreply.github.com>
2025-04-04 11:33:42 +0000

    object  merging

---
+++ 02_basic/04_objects.js
@@ -0,0 +1,35 @@
+// const facebookUser = new Object ()   **singleton object
+
+const facebookUser = {}   //Non singleton Objects 
+
+facebookUser.name = "Parth"
+facebookUser.age = 23
+facebookUser.loc = "Maharashtra"
+facebookUser.id = "125689"
+
+// console.log(facebookUser);
+
+const regularUser = {
+    email : "someuser@gmail.com",
+    fullname : {
+        userfullname : {
+            firstname: "Parth ",
+            lastname : "Deshmukh",
+            
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+//Merging Objects
+
+const obj1 = {1: "a", 2:"b"}
+const obj2 = {3:"c", 4: "d"}
+
+// const obj3 = {obj1, obj2}
+const obj3 = Object.assign({}, obj1,obj2)
+console.log(obj3);
+
+
+
