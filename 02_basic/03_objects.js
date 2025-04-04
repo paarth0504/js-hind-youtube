65d50743aa71509e4f2d97f59b2cd9c8fa3beca4
Parth <94096881+paarth0504@users.noreply.github.com>
2025-04-04 09:28:54 +0000

    object

---
+++ 02_basic/03_objects.js
@@ -0,0 +1,13 @@
+
+const JsUser = {
+    name : "Parth",
+    age : 23,
+    location : "Maharashtra",
+    cont : 9049886898,
+    email : "paarthdeshmukh71@gmail.com",
+    isLoggedIn: false
+}
+
+console.log(JsUser.age);
+
+
