36884b5babe0089f44f69662bb9c750350a0b88c
Parth <94096881+paarth0504@users.noreply.github.com>
2025-04-04 12:07:46 +0000

    objects part 2

--- 02_basic/04_objects.js
+++ 02_basic/04_objects.js
@@ -7,6 +7,14 @@ facebookUser.age = 23
 facebookUser.loc = "Maharashtra"
 facebookUser.id = "125689"
 
+console.log(Object.keys(facebookUser));
+// console.log((Object.values(facebookUser.id)));
+// console.log((Object.entries(facebookUser)));
+console.log(facebookUser.hasOwnProperty(`idff`));
+
+
+
+
 // console.log(facebookUser);
 
 const regularUser = {
@@ -28,8 +36,15 @@ const obj1 = {1: "a", 2:"b"}
 const obj2 = {3:"c", 4: "d"}
 
 // const obj3 = {obj1, obj2}
-const obj3 = Object.assign({}, obj1,obj2)
-console.log(obj3);
+// const obj3 = Object.assign({}, obj1,obj2)
+// console.log(obj3);
+
+const obj3 = {...obj1,...obj2} //Spreading objects
+// console.log(obj3);
+
+//video 17 cmplted
+
+
 
 
 
