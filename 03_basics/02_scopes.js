debb9e0b46dd9782666859fcf4bad04ced630fd0
Parth <94096881+paarth0504@users.noreply.github.com>
2025-04-07 09:56:22 +0000

    About Scopes L1

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,13 @@
+var c = 300
+let a = 300
+if (true) {
+    let a = 23
+    const b = 55
+    // console.log("Inner  Value : " ,a);
+
+}
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
