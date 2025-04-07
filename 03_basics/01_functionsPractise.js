922f2972bbed4a8ca85771dcfb1e13af5a6060bd
Parth <94096881+paarth0504@users.noreply.github.com>
2025-04-07 10:31:37 +0000

    practise

---
+++ 03_basics/01_functionsPractise.js
@@ -0,0 +1,23 @@
+function sayMyName(params) {
+    console.log("Parth");
+    console.log("Deshmukh");
+
+}
+
+// sayMyName()
+
+function addNumber(numb1,numb2) {
+    // console.log(numb1+numb2);
+    
+}
+// addNumber(5,5)
+
+function addingNum(num1,num2) {
+    return num1 + num2
+    
+}
+
+const resultAdd = addingNum(20,20)
+console.log("Result of adding Number is :" , resultAdd);
+
+
