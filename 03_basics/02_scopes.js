8d99d10e0ca4a9f97f9663fa1f29ff9c71520b03
Parth <94096881+paarth0504@users.noreply.github.com>
2025-04-07 11:24:54 +0000

    Scope level and Mini hosting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -10,4 +10,48 @@ if (true) {
 // console.log(b);
 // console.log(c);
 
+function one(params) {
+    const username = "Parth"
+
+    function two(params) {
+        const website = "Youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+    
+    // two()
+}
+// one()
+if (true) {
+    const username ="Parth"
+    if (username==="Parth") {
+        const website = "YoutUBE"
+        // console.log( username + website);
+        
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+//+++++++++++  Intresting Example +++++ 
+addOne(5)
+function addOne(num) {
+    return num + 1
+
+}
+
+
+const addTwo = function addTwo(num) {
+    return num + 1
+
+}
+
+addTwo(5)
+
+//Scope Level And Mini Hosting Vid- 22 Cmplted
+
+
 
