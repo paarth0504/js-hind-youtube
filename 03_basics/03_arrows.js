0c3461e9861eb6e821da20797ed5b0c9a636add2
Parth <94096881+paarth0504@users.noreply.github.com>
2025-04-08 09:41:22 +0000

    about arrow funtion

--- 03_basics/03_arrows.js
+++ 03_basics/03_arrows.js
@@ -18,4 +18,22 @@ function chai() {
     
 }
 
-chai() 
+// chai() 
+
+// ***** arrow function *****
+
+// const addTwo = (num1, num2) =>{
+// return num1 * num2
+// }
+
+// console.log(addTwo());
+
+//*****Implicit return ***** */
+
+// const addTwo = (num1 , num2)=> num1 * num2 
+// const addTwo = (num1 , num2)=> ( num1 * num2 ) //This Techinque Is USeful While Learning React
+const addTwo = (num1 , num2)=> ({username: "Parth D"})//if u have to return an object then you have to wrap in ()
+console.log(addTwo(2,2));
+
+
+
