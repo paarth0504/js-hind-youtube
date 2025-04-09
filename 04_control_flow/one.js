4e3f0b8a22b3cb64eec3e7f916c1db90c5d65a49
Parth <94096881+paarth0504@users.noreply.github.com>
2025-04-09 07:03:07 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,43 @@
+
+// if
+
+const isuserLoggedIn = true
+
+if (2 != 3) {
+    // console.log("Executed");
+    
+}
+
+// const temprature = 41
+
+// if (temprature < 40) {
+//     console.log("Temprature Is Less than 41");
+// } else{
+//     console.log("Temperature Is More Than 41");
+// } 
+
+// console.log("Executed");
+// < , > , <= , >= , == , != , === , !==
+
+
+// Blog Scope 
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = 'Fly'
+//     console.log( `User Power : ${ power}` );
+    
+// }
+    // console.log(`User Power : ${ power}`); 
+    
+// Implicit Scope 
+
+const balance = 2000
+
+if(balance>500) console.log("test"); // Implicit Scope
+
+// if(balance>500) console.log("test") , console.log("test 2") , console.log("test 3"); Immature Habit Of Executing The Code.
+
+
+
