0074cc05a12f53ba1b2caabb4968c4320f50117c
Parth <94096881+paarth0504@users.noreply.github.com>
2025-04-09 10:33:54 +0000

    Switch stmt

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,68 @@
+// switch (key) {
+//     case value:
+        
+//         break;    // ** Switch Case Syntax
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+
+// switch (month) {
+//     case 1 :
+//         console.log("january");
+        
+//         break;
+
+//         case 2 :
+//         console.log("february");
+        
+//         break;
+
+//         case 3 :
+//         console.log("march");
+        
+//         break;
+
+//         case 4 :
+//         console.log("april");
+        
+//         break;
+
+//     default:
+//         console.log("Default Case Matched");
+        
+//         break;
+// }
+
+// ***** string example *****
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("january");
+        
+        break;
+
+        case "feb":
+        console.log("february");
+        
+        break;
+
+        case "march" :
+        console.log("march");
+        
+        break;
+
+        case "april" :
+        console.log("april");
+        
+        break;
+
+    default:
+        console.log("Default Case Matched");
+        
+        break;
+}
