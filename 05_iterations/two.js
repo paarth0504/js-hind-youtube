ae02e4d04ce6ed9973cc8ffcd61f6c70ef34b497
Parth <94096881+paarth0504@users.noreply.github.com>
2025-04-11 06:04:29 +0000

    about while , dowhile bsics

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+// *** while loop **
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is : ${index}`);
+//     index = index + 2 
+    
+// }
+
+// let myArray = ["Parth", "parth" , "pArTh" , "htrap"]
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`value of my array is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+// *** do while 
+let score = 11
+
+do {
+    console.log(`score is : ${ score}`);
+    score++
+} while (score<=10);
