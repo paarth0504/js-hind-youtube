070aafe2af58d22c8e6c33947fcd2912eb266bbd
Parth <94096881+paarth0504@users.noreply.github.com>
2025-04-11 07:07:21 +0000

    map

---
+++ 05_iterations/three.js
@@ -0,0 +1,41 @@
+// for of
+
+const arr = [1,2,3,4,5,6,7,8,9]
+
+for (const num of arr) {
+    // console.log(`${num}`);
+    
+}
+
+const greeting  = "Hello World"
+
+for (const greet of greeting) {
+    // console.log(`Each Character Is ${greet}`);
+    
+}
+
+// maps
+
+const map = new Map()
+map.set('BH' , "BHARAT")
+map.set('US' , "United States")
+map.set('FR' , "France")
+map.set('GR' , "Germany")
+
+// console.log(map);
+
+for (const [key , value] of map) {
+    // console.log(key , ":-" , value);
+    
+}
+
+const myObject={
+    game1 : "NFS- Most Wanted",
+    game2 : "Grand Theft Auto"
+}
+
+// for (const [key , value] of myObject) {
+//     console.log(key , ":-" , value);        // not iterable
+    
+// }
+
